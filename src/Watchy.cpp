580506a665fe4036ed6ab5c066aa11bf099672e4
Nikolai Gubarev <BEEFC0DEh@gmail.com>
2024-08-11 00:48:36 +0300

    Reduce number of full screen updates.

--- src/Watchy.cpp
+++ src/Watchy.cpp
@@ -57,7 +57,7 @@ void Watchy::init(String datetime) {
       // Return to watchface if in menu for more than one tick
       if (alreadyInMenu) {
         guiState = WATCHFACE_STATE;
-        showWatchFace(false);
+        showWatchFace(true);
       } else {
         alreadyInMenu = true;
       }
@@ -138,7 +138,7 @@ void Watchy::handleButtonPress() {
   if (wakeupBit & MENU_BTN_MASK) {
     if (guiState ==
         WATCHFACE_STATE) { // enter menu state if coming from watch face
-      showMenu(menuIndex, false);
+      showMenu(menuIndex, true);
     } else if (guiState ==
                MAIN_MENU_STATE) { // if already in menu, then select menu item
       switch (menuIndex) {
@@ -174,11 +174,11 @@ void Watchy::handleButtonPress() {
   else if (wakeupBit & BACK_BTN_MASK) {
     if (guiState == MAIN_MENU_STATE) { // exit to watch face if already in menu
       RTC.read(currentTime);
-      showWatchFace(false);
+      showWatchFace(true);
     } else if (guiState == APP_STATE) {
-      showMenu(menuIndex, false); // exit to menu if already in app
+      showMenu(menuIndex, true); // exit to menu if already in app
     } else if (guiState == FW_UPDATE_STATE) {
-      showMenu(menuIndex, false); // exit to menu if already in app
+      showMenu(menuIndex, true); // exit to menu if already in app
     } else if (guiState == WATCHFACE_STATE) {
       return;
     }
@@ -256,12 +256,12 @@ void Watchy::handleButtonPress() {
         if (guiState ==
             MAIN_MENU_STATE) { // exit to watch face if already in menu
           RTC.read(currentTime);
-          showWatchFace(false);
+          showWatchFace(true);
           break; // leave loop
         } else if (guiState == APP_STATE) {
-          showMenu(menuIndex, false); // exit to menu if already in app
+          showMenu(menuIndex, true); // exit to menu if already in app
         } else if (guiState == FW_UPDATE_STATE) {
-          showMenu(menuIndex, false); // exit to menu if already in app
+          showMenu(menuIndex, true); // exit to menu if already in app
         }
       } else if (digitalRead(UP_BTN_PIN) == ACTIVE_LOW) {
         lastTimeout = millis();
@@ -292,7 +292,6 @@ void Watchy::showMenu(byte menuIndex, bool partialRefresh) {
 }
 
 void Watchy::showFastMenu(byte menuIndex, bool partialRefresh) {
-  display.setFullWindow();
   display.fillScreen(bgColor());
   display.setFont(&FreeMonoBold9pt7b);
 
@@ -324,7 +323,6 @@ void Watchy::showFastMenu(byte menuIndex, bool partialRefresh) {
 }
 
 void Watchy::showAbout() {
-  display.setFullWindow();
   display.fillScreen(bgColor());
   display.setFont(&FreeMonoBold9pt7b);
   display.setTextColor(fgColor());
@@ -367,21 +365,20 @@ void Watchy::showAbout() {
   }else{
     display.println("WiFi Not Connected");
   }
-  display.display(false); // full refresh
+  display.display(true);
 
   guiState = APP_STATE;
 }
 
 void Watchy::showBuzz() {
-  display.setFullWindow();
   display.fillScreen(bgColor());
   display.setFont(&FreeMonoBold9pt7b);
   display.setTextColor(fgColor());
   display.setCursor(70, 80);
   display.println("Buzz!");
-  display.display(false); // full refresh
+  display.display(true);
   vibMotor();
-  showMenu(menuIndex, false);
+  showMenu(menuIndex, true);
 }
 
 void Watchy::vibMotor(uint8_t intervalMs, uint8_t length) {
@@ -561,11 +558,10 @@ void Watchy::setTime() {
 
   RTC.set(tm);
 
-  showMenu(menuIndex, false);
+  showMenu(menuIndex, true);
 }
 
 void Watchy::showAccelerometer() {
-  display.setFullWindow();
   display.fillScreen(bgColor());
   display.setFont(&FreeMonoBold9pt7b);
   display.setTextColor(fgColor());
@@ -633,7 +629,7 @@ void Watchy::showAccelerometer() {
     }
   }
 
-  showMenu(menuIndex, false);
+  showMenu(menuIndex, true);
 }
 
 void Watchy::showWatchFace(bool partialRefresh) {
@@ -888,7 +884,6 @@ void Watchy::setupWifi() {
   wifiManager.resetSettings();
   wifiManager.setTimeout(WIFI_AP_TIMEOUT);
   wifiManager.setAPCallback(_configModeCallback);
-  display.setFullWindow();
   display.fillScreen(bgColor());
   display.setFont(&FreeMonoBold9pt7b);
   display.setTextColor(fgColor());
@@ -904,7 +899,7 @@ void Watchy::setupWifi() {
     lastIPAddress = WiFi.localIP();
     WiFi.SSID().toCharArray(lastSSID, 30);
   }
-  display.display(false); // full refresh
+  display.display(true); // full refresh
   // turn off radios
   WiFi.mode(WIFI_OFF);
   btStop();
@@ -926,7 +921,7 @@ void Watchy::_configModeCallback(WiFiManager *myWiFiManager) {
   display.println(WiFi.softAPIP());
 	display.println("MAC address:");
 	display.println(WiFi.softAPmacAddress().c_str());
-  display.display(false); // full refresh
+  display.display(true);
 }
 
 bool Watchy::connectWiFi() {
@@ -1053,7 +1048,7 @@ void Watchy::updateFWBegin() {
   // turn off radios
   WiFi.mode(WIFI_OFF);
   btStop();
-  showMenu(menuIndex, false);
+  showMenu(menuIndex, true);
 }
 
 void Watchy::showSyncNTP() {
@@ -1097,7 +1092,7 @@ void Watchy::showSyncNTP() {
   }
   display.display(true); // full refresh
   delay(3000);
-  showMenu(menuIndex, false);
+  showMenu(menuIndex, true);
 }
 
 bool Watchy::syncNTP() { // NTP sync - call after connecting to WiFi and
